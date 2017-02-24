45527f455fec50769c948b5830325eda1cadbc8d
Alan Jeferson <alan.jeferson11@gmail.com>
2017-02-24 19:47:42 +0000

    Make test to fail

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
 	it 'test failure' do
-		#str = 'Test'
-		#expect(str).to eq('Kakaroto')
+		str = 'Test'
+		expect(str).to eq('Kakaroto')
 	end
 
 end
